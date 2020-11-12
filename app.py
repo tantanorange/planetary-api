0ee7588fa12b9deac9cd2e5775eee8901753648e
tantanorange <44329336+tantanorange@users.noreply.github.com>
2020-11-11 23:18:36 -0500

    Set up url format & Create Database.

--- app.py
+++ app.py
@@ -1,6 +1,65 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 app = Flask(__name__)
+# 把base directory放在和项目文件同样的地址下
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+# activate database
+db = SQLAlchemy(app)
+
+
+# creation
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print("DB created")
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print("DB dropped")
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    venus = Planet(planet_name='Venus',
+                   planet_type='Class K',
+                   home_star='Sol',
+                   mass=4.867e24,
+                   radius=3760,
+                   distance=67.24e6)
+
+    earth = Planet(planet_name='Earth',
+                   planet_type='Class M',
+                   home_star='Sol',
+                   mass=5.972e24,
+                   radius=3959,
+                   distance=92.96e7)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name='William',
+                     last_name='Herschel',
+                     email='test@gmail.com',
+                     password='password')
+    db.session.add(test_user)
+    # very important
+    db.session.commit()
+    print('Database seeded!')
 
 
 @app.route('/')
@@ -10,7 +69,53 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from Planetary API.'
+    return jsonify(message='Hello from Planetary API.'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message="sorry" + name + ", underage. NO!"), 401
+    else:
+        return jsonify(message="good to go")
+
+
+# string is a flask function. This a called variable rule matching
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="sorry" + name + ", underage. NO!"), 401
+    else:
+        return jsonify(message="good to go")
+
+
+# database models
+# specify to use db.model lib to create this class
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == '__main__':
